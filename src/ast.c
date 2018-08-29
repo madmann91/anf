401b4c50e24cb2f9b002ef538f665d9bea324eb4
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2018-08-29 18:46:30 +0200

    More AST nodes

--- src/ast.c
+++ src/ast.c
@@ -1,57 +1,6 @@
 #include "ast.h"
 
-bool ast_is_decl(const ast_t* ast) {
-    switch (ast->tag) {
-        case AST_DEF:
-        case AST_VAR:
-        case AST_VAL:
-        case AST_MOD:
-            return true;
-        default:
-            return false;
-    }
-}
-
-bool ast_is_expr(const ast_t* ast) {
-    ast_list_t* list;
-    switch (ast->tag) {
-        case AST_ID:
-        case AST_LIT:
-            return true;
-        case AST_TUPLE:
-            list = ast->data.tuple.args;
-            while (list) {
-                if (!ast_is_expr(list->ast))
-                    return false;
-                list = list->next;
-            }
-            return true;
-        default:
-            return false;
-    }
-}
-
-bool ast_is_ptrn(const ast_t* ast) {
-    ast_list_t* list;
-    switch (ast->tag) {
-        case AST_ID:
-        case AST_LIT:
-            return true;
-        case AST_TUPLE:
-            list = ast->data.tuple.args;
-            while (list) {
-                if (!ast_is_ptrn(list->ast))
-                    return false;
-                list = list->next;
-            }
-            return true;
-        default:
-            return false;
-    }
-}
-
 bool ast_is_refutable(const ast_t* ast) {
-    assert(ast_is_ptrn(ast));
     ast_list_t* list;
     switch (ast->tag) {
         case AST_ID:  return false;
--- src/ast.h
+++ src/ast.h
@@ -16,7 +16,55 @@ enum ast_tag_e {
     AST_VAL,
     AST_BLOCK,
     AST_TUPLE,
+    AST_BINOP,
+    AST_UNOP,
+    AST_LAMBDA,
+    AST_IF,
+    AST_MATCH,
+    AST_WHILE,
+    AST_FOR,
     AST_ERR
+};
+
+enum unop_tag_e {
+    UNOP_NEG,
+    UNOP_PLUS,
+    UNOP_PRE_INC,
+    UNOP_PRE_DEC,
+    UNOP_POST_INC,
+    UNOP_POST_DEC,
+    UNOP_DEREF,
+    UNOP_TAKE_ADDR
+};
+
+enum binop_tag_e {
+    BINOP_ADD,
+    BINOP_SUB,
+    BINOP_MUL,
+    BINOP_DIV,
+    BINOP_REM,
+    BINOP_AND,
+    BINOP_OR,
+    BINOP_XOR,
+    BINOP_LSHFT,
+    BINOP_RSHFT,
+    BINOP_ASSIGN_ADD,
+    BINOP_ASSIGN_SUB,
+    BINOP_ASSIGN_MUL,
+    BINOP_ASSIGN_DIV,
+    BINOP_ASSIGN_REM,
+    BINOP_ASSIGN_AND,
+    BINOP_ASSIGN_OR,
+    BINOP_ASSIGN_XOR,
+    BINOP_ASSIGN_LSHFT,
+    BINOP_ASSIGN_RSHFT,
+    BINOP_LOGIC_AND,
+    BINOP_LOGIC_OR,
+    BINOP_EQ,
+    BINOP_LT,
+    BINOP_GT,
+    BINOP_GE,
+    BINOP_LE
 };
 
 struct ast_s {
@@ -49,7 +97,34 @@ struct ast_s {
         struct {
             ast_list_t* args;
         } tuple;
+        struct {
+            uint32_t    tag;
+            ast_t*      left;
+            ast_t*      right;
+        } binop;
+        struct {
+            uint32_t    tag;
+            ast_t*      op;
+        } unop;
+        struct {
+            ast_t*      param;
+            ast_t*      body;
+        } lambda;
+        struct {
+            ast_t*      if_true;
+            ast_t*      if_false;
+        } if_;
+        struct {
+            ast_t*      cond;
+            ast_t*      body;
+        } while_;
+        struct {
+            ast_t*      vars;
+            ast_t*      call;
+            ast_t*      body;
+        } for_;
     } data;
+    const type_t* type;
     loc_t loc;
 };
 
@@ -58,9 +133,6 @@ struct ast_list_s {
     ast_list_t* next;
 };
 
-bool ast_is_decl(const ast_t*);
-bool ast_is_expr(const ast_t*);
-bool ast_is_ptrn(const ast_t*);
 bool ast_is_refutable(const ast_t*);
 
 void ast_print(const ast_t*, size_t, bool);
