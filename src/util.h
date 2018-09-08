bca925ef313219776bb584e29ff976916bef19c2
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2018-09-09 00:23:22 +0200

    Handle undefined behaviour for 0-sized allocs

--- src/util.h
+++ src/util.h
@@ -81,16 +81,20 @@ static inline void die(const char* msg) {
 }
 
 static inline void* xmalloc(size_t size) {
+    if (size == 0)
+        return NULL;
     void* ptr = malloc(size);
     if (!ptr)
         die("out of memory, malloc() failed");
 #ifndef NDEBUG
-    memset(ptr, 0xA5, size);
+    memset(ptr, 0xC4, size);
 #endif
     return ptr;
 }
 
 static inline void* xcalloc(size_t num, size_t size) {
+    if (size == 0 || num == 0)
+        return NULL;
     void* ptr = calloc(num, size);
     if (!ptr)
         die("out of memory, calloc() failed");
@@ -98,6 +102,10 @@ static inline void* xcalloc(size_t num, size_t size) {
 }
 
 static inline void* xrealloc(void* ptr, size_t size) {
+    if (size == 0) {
+        free(ptr);
+        return NULL;
+    }
     ptr = realloc(ptr, size);
     if (!ptr)
         die("out of memory, realloc() failed");
